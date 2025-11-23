725628cc2f4f42dc085771eaf30da90babb9d883
kunal <kunalkumar151123@gmail.com>
2025-11-23 21:55:19 +0530

    Add code_forces folder

---
+++ code_forces/ban_ban.java
@@ -0,0 +1,24 @@
+//Codeforces Round 832 (Div. 2) page 4
+
+package code_forces;
+
+import java.util.Scanner;
+
+public class ban_ban {
+    
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-->0){
+            int n= sc.nextInt();
+            int i = 2;
+            int j=3*n;
+            System.out.println((n+1)/2);
+            while(i<j){
+                System.out.println(i+" "+j);
+                i = i+3;
+                j = j+3;
+            }
+        }
+    }
+}
